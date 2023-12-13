5f29cc87ad4c4915fb5a3a042db2b7bf2dad1806
mtlubins <mtlubins@live.com>
2023-12-13 16:25:36 +0100

    add backend integration to app

--- app/src/index.ts
+++ app/src/index.ts
@@ -3,12 +3,21 @@ import './app.css';
 function main(): void {
     const button = document.querySelector("#testButton");
     button?.addEventListener("click", testSubmitHandler);
+    document.querySelector("#testButtonFunc")?.addEventListener("click", testSubmitFuncHandler);
 }
 
 function testSubmitHandler(e: Event): void {
     e.preventDefault();
     const outputElement = document.querySelector('#output');
     outputElement.textContent = 'Scripts work!';
+}
+
+async function testSubmitFuncHandler(e: Event): Promise<void> {
+    e.preventDefault();
+    const apiResponse = await fetch(`/api/example-service`);
+    const apiResonseText = await apiResponse.text();
+    const outputElement = document.querySelector('#outputFunc');
+    outputElement.textContent = apiResonseText;
 }
 
 main();
